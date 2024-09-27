49abfe327245de402d942604250028fc3330ba70
danfoley0 <danieljfoley@icloud.com>
2024-09-27 12:35:07 -0400

    placement and pathblocker working

--- main.gd
+++ main.gd
@@ -1,15 +1,20 @@
 extends Node2D
-
 @export var turret1scene : PackedScene
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	 # Replace with function body.
-	pass
+var turretPermission = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	if Input.is_action_just_pressed("left_click"):
+	if Input.is_action_just_pressed("left_click") and turretPermission == true:
 		var turret : Node2D = turret1scene.instantiate()
 		turret.global_position = get_global_mouse_position()
 		self.add_child(turret)
+
+
+func _on_area_2d_mouse_entered():
+	turretPermission = false
+	pass # Replace with function body.
+
+
+func _on_area_2d_mouse_exited():
+	turretPermission = true
+	pass # Replace with function body.
